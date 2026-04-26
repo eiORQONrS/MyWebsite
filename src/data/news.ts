574fad52c93ee32c1e37a9c5fe7face215fb074d
Yanjun Chen <cyj123qq@email.com>
2026-04-26 14:59:47 -0500

    News text justify alignment

--- src/data/news.ts
+++ src/data/news.ts
@@ -2,11 +2,15 @@
 // Add newest items at the top of the array.
 export const news: { date: string; text: string }[] = [
   {
-    date: "Apr 2026",
-    text: "Strain2Force is a Finalist for Best Conference Technical Paper Award at IEEE Haptics Symposium 2026.",
+  date: "4/6/2026",
+  text: "Published “Sensitivity at the Edges of the Finger Enables Mixed Reality Force Feedback” in IEEE Transactions on Haptics.",
   },
   {
-    date: "Apr 2026",
-    text: "Received Distinguished Demonstration Award at IEEE Haptics Symposium 2026.",
+    date: "4/1/2026",
+    text: "Received the Distinguished Demonstration Award at IEEE Haptics Symposium 2026.",
+  },
+  {
+    date: "3/29/2026",
+    text: "Strain2Force is a Finalist for Best Conference Technical Paper Award at IEEE Haptics Symposium 2026.",
   },
 ];
