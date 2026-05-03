36bd2d316160c8fafc6fe2b67a7c6f4c079b3216
Yanjun Chen <cyj123qq@email.com>
2026-05-02 20:27:30 -0500

    Add GitHub icon to hero links

--- src/config.ts
+++ src/config.ts
@@ -25,6 +25,7 @@ export const site = {
     cvUrl: '/uploads/Yanjun_Chen.pdf',
     googleScholarUrl: 'https://scholar.google.com/citations?user=EvPrj_QAAAAJ',
     linkedinUrl: 'https://www.linkedin.com/in/yanjunc/',
+    githubUrl: 'https://github.com/YanjunChenPocket',
     email: 'yanjunc2@illinois.edu',
     bio: `I am a second-year Ph.D. student in <a href="https://ece.illinois.edu/" target="_blank" rel="noopener">Electrical and Computer Engineering</a> at the University of Illinois Urbana-Champaign, working with <a href="https://www.craig-shultz.com/" target="_blank" rel="noopener">Prof. Craig Shultz</a> in the <a href="https://www.interactivedisplaylab.com/" target="_blank" rel="noopener">Interactive Display Lab</a>. Previously, I served as a Research Assistant at the <a href="http://english.is.cas.cn/" target="_blank" rel="noopener">Institute of Software, Chinese Academy of Sciences</a>, under the guidance of <a href="https://scholar.google.co.jp/citations?user=kHKwQ9gAAAAJ&hl=en" target="_blank" rel="noopener">Prof. Teng Han</a>. I received my B.E. in Computer Science and Technology from Chengdu University of Information Technology, China.`,
     interests: `My research interests lie in the area of human-computer interaction (HCI). I am currently exploring haptic feedback and novel sensing technologies, including compact actuators/sensors, wearables, and interactive interfaces.`,
